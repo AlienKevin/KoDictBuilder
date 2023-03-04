d4f786753c8ebec5ddb30bb0d06f25fa36742532
Kevin Li <kevinli020508@gmail.com>
2023-03-04 13:11:34 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using MessagePack;
+
+class Build
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine($"Require 1 argument, but got {args.Length}");
+        }
+        else if (args[0] == "dict")
+        {
+            BuildDict();
+        }
+        else if (args[0] == "index")
+        {
+            BuildKoIndex();
+        }
+        else
+        {
+            Console.WriteLine($"Invalid argument {args[0]}");
+        }
+    }
+
+    static void BuildDict()
+    {
+        using (StreamReader r = new StreamReader("../KoDict/dict.json"))
+        {
+            string json = r.ReadToEnd();
+            var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4Block);
+            File.WriteAllBytes(path: "../KoDict/dict.msgpack.l4z", MessagePackSerializer.ConvertFromJson(json, lz4Options));
+        }
+    }
+
+    static void BuildKoIndex()
+    {
+        var dict = new Dict();
+        foreach (var entry in dict.Entries)
+        {
+            var jamos = Hangeul.Hangeuls2Jamos(entry.Word);
+            ulong hash64 = Farmhash.Sharp.Farmhash.Hash64("Hello");
+            // TODO
+        }
+    }
+}
