43678a48565d9a551c5ac5334604275ed11fa314
Kevin Li <kevinli020508@gmail.com>
2023-03-05 19:40:35 -0500

    Build english index

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ class Build
         else if (args[0] == "index")
         {
             BuildKoIndex();
+            BuildEnIndex();
         }
         else
         {
@@ -51,21 +52,53 @@ class Build
         var dict = new Dict();
         var d0Neighborhood = new Dictionary<ulong, List<int>>();
         var d1Neighborhood = new Dictionary<ulong, List<int>>();
-        for (int i = 0; i < dict.Entries.Count; i++)
+        for (int entryIndex = 0; entryIndex < dict.Entries.Count; entryIndex++)
         {
-            var entry = dict.Entries[i];
+            var entry = dict.Entries[entryIndex];
             var jamos = Hangeul.Hangeuls2Jamos(entry.Word);
             var wordHash = Farmhash.Sharp.Farmhash.Hash64(jamos);
-            AddItem(d0Neighborhood, wordHash, i);
+            AddItem(d0Neighborhood, wordHash, entryIndex);
             // 1-deletion-neighborhood
-            AddItem(d1Neighborhood, wordHash, i);
+            AddItem(d1Neighborhood, wordHash, entryIndex);
             for (int k = 0; k < jamos.Count(); k++)
             {
                 var d1WordHash = Farmhash.Sharp.Farmhash.Hash64(jamos.Remove(k, 1));
-                AddItem(d1Neighborhood, d1WordHash, i);
+                AddItem(d1Neighborhood, d1WordHash, entryIndex);
             }
         }
         File.WriteAllBytes(path: "../KoDict/d0Neighborhood.msgpack", MessagePackSerializer.Serialize(d0Neighborhood));
         File.WriteAllBytes(path: "../KoDict/d1Neighborhood.msgpack", MessagePackSerializer.Serialize(d1Neighborhood));
+    }
+
+    static void BuildEnIndex()
+    {
+        var dict = new Dict();
+        var d0Neighborhood = new Dictionary<ulong, List<int>>();
+        var d1Neighborhood = new Dictionary<ulong, List<int>>();
+        for (int entryIndex = 0; entryIndex < dict.Entries.Count; entryIndex++)
+        {
+            var senses = dict.Entries[entryIndex].Senses.Select(sense => sense.EnglishLemma.ToLower());
+            foreach (var (sense, senseIndex) in senses.Select((sense, senseIndex) => (sense, senseIndex)))
+            {
+                foreach (var word in sense.Split("; "))
+                {
+                    var index = Dict.PackEntryAndSenseIndex(entryIndex, senseIndex);
+                    var wordHash = Farmhash.Sharp.Farmhash.Hash64(word);
+                    AddItem(d0Neighborhood, wordHash, index);
+                    // 1-deletion-neighborhood
+                    AddItem(d1Neighborhood, wordHash, index);
+                    if (word.Count() >= 3)
+                    {
+                        for (int k = 0; k < word.Count(); k++)
+                        {
+                            var d1WordHash = Farmhash.Sharp.Farmhash.Hash64(word.Remove(k, 1));
+                            AddItem(d1Neighborhood, d1WordHash, index);
+                        }
+                    }
+                }
+            }
+        }
+        File.WriteAllBytes(path: "../KoDict/enD0Neighborhood.msgpack", MessagePackSerializer.Serialize(d0Neighborhood));
+        File.WriteAllBytes(path: "../KoDict/enD1Neighborhood.msgpack", MessagePackSerializer.Serialize(d1Neighborhood));
     }
 }
